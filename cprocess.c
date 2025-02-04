c755e9f6858619a6cb3e135f1d9dfbbf4a9cd72f
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-02-04 13:38:47 +0000

    **Added** default function implementations pointed to by lexer function pointers

--- cprocess.c
+++ cprocess.c
@@ -30,3 +30,45 @@ struct compile_process *compile_process_create(const char *filename,
 
     return process;
 }
+
+/*
+ * Default functions pointed at from lexer funtion pointers.
+ * */
+
+
+/*
+ * Method to get the next char from the filestream.
+ * */
+char compile_process_next_char(struct lex_process* lex_process) {
+    struct compile_process* compiler = lex_process->compiler;
+    compiler->pos.col += 1; // Moves to the next column
+    char c = getc(compiler->cfile.fp); // Pulls character from filestream
+    
+    // If character is new line symbol increment line counter and set column counter to 0
+    if(c == '\n') {
+        compiler->pos.line += 1;
+        compiler->pos.col = 0;
+    }
+
+    return c;
+}
+
+
+/*
+ * Method to peek at the next char in the filestream
+ * */
+char compile_process_peek_char(struct lex_process* lex_process) {
+    struct compile_process* compiler = lex_process-> compiler;
+    char c = getc(compiler->cfile.fp); // Pulls char from filestream
+    ungetc(c, compiler->cfile.fp); // Push character back to filestream
+    return c;
+}
+
+
+/*
+ * Method to push a a new char on to the filestream
+ * */
+void compile_process_push_char(struct lex_process* lex_process, char c) {
+    struct compile_process* compiler = lex_process-> compiler;
+    ungetc(c, compiler->cfile.fp); // Pushes provided char to the file stream
+}

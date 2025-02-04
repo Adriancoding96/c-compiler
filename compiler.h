f86e3a8cf52ab3a7f2e354a84e24892d95fe337c
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-02-04 13:41:20 +0000

    **Added** Function prototypes for defualt lexer function implementations in compiler.h

--- compiler.h
+++ compiler.h
@@ -102,4 +102,11 @@ int compile_file(const char* filename, const char* out_filename, int flags);
 struct compile_process *compile_process_create(const char *filename,
                                                const char *filename_out,
                                                int flags);
+
+// Function prototypes for default lexer function implementations
+char compile_process_next_char(struct lex_process* lex_process); 
+
+char compile_process_peek_char(struct lex_process* lex_process);
+
+void compile_process_push_char(struct lex_process* lex_process, char c);
 #endif

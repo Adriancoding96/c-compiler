0c0ea06fbeb00f4fbc6094f97f4e115c281f8e3a
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-02-05 13:08:34 +0000

    **Fixed** gramatical errors in function definitions

--- compiler.c
+++ compiler.c
@@ -3,7 +3,7 @@
 /*
  * Defines lexer functions.
  * */
-struct lex_process_functions compiler_lex_function = {
+struct lex_process_functions compiler_lex_functions = {
     .next_char=compile_process_next_char,
     .peek_char=compile_process_peek_char,
     .push_char=compile_process_push_char,
@@ -19,6 +19,17 @@ int compile_file(const char* filename, const char* out_filename, int flags) {
         return COMPILER_FAILED_WITH_ERRORS;
     }
 
+    struct lex_process* lex_process = lex_process_create(process, &compiler_lex_functions, NULL);
+
+    // Make sure creation of lex process was successfull
+    if(!lex_process) {
+        return COMPILER_FAILED_WITH_ERRORS;
+    }
+
+    // Make sure lexer successufully runs
+    if(lex(lex_process) != LEXICAL_ANALYSIS_ALL_OK) {
+        return COMPILER_FAILED_WITH_ERRORS;
+    }
 
     // Return enum representing successfull compile process
     return COMPILER_FILE_COMPILED_OK;

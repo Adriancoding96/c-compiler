8907e77aad08b9a5c3088808d79be28f806469fe
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-02-05 11:48:03 +0000

    **Fixed** error in compile_process_next_char function regarding column position

--- cprocess.c
+++ cprocess.c
@@ -47,7 +47,7 @@ char compile_process_next_char(struct lex_process* lex_process) {
     // If character is new line symbol increment line counter and set column counter to 0
     if(c == '\n') {
         compiler->pos.line += 1;
-        compiler->pos.col = 0;
+        compiler->pos.col = 1;
     }
 
     return c;

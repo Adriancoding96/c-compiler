395e6863a9b8527057c471558034380f1c0d9479
Adrian Nilsson <adrian.nilsson.coding@gmail.com>
2025-02-06 15:33:17 +0000

    **Added** tokenisation of keywords

--- lexer.c
+++ lexer.c
@@ -292,7 +292,43 @@ bool lex_is_in_expression() {
     return lex_process->current_expression_count > 0;
 }
 
-
+/*
+ * Function checks if string equals a keyword
+ * */
+bool is_keyword(const char* str) {
+    return S_EQ(str, "unsigned")
+        || S_EQ(str, "signed")
+        || S_EQ(str, "char")
+        || S_EQ(str, "short")
+        || S_EQ(str, "int")
+        || S_EQ(str, "long")
+        || S_EQ(str, "float")
+        || S_EQ(str, "double")
+        || S_EQ(str, "void")
+        || S_EQ(str, "struct")
+        || S_EQ(str, "union")
+        || S_EQ(str, "static")
+        || S_EQ(str, "__ignore_typecheck")
+        || S_EQ(str, "return")
+        || S_EQ(str, "include")
+        || S_EQ(str, "sizeof")
+        || S_EQ(str, "if")
+        || S_EQ(str, "else")
+        || S_EQ(str, "while")
+        || S_EQ(str, "for")
+        || S_EQ(str, "do")
+        || S_EQ(str, "break")
+        || S_EQ(str, "continue")
+        || S_EQ(str, "switch")
+        || S_EQ(str, "case")
+        || S_EQ(str, "case")
+        || S_EQ(str, "default")
+        || S_EQ(str, "goto")
+        || S_EQ(str, "typedef")
+        || S_EQ(str, "const")
+        || S_EQ(str, "extern")
+        || S_EQ(str, "restrict");
+}
 
 /*
  * Function creates a operator token with the exceptions of #include statements
@@ -347,6 +383,10 @@ static struct token* token_make_identifier_or_keyword() {
     buffer_write(buf, 0x00);
 
     // Check if keyword
+    if(is_keyword(buffer_ptr(buf))) { 
+        // Returns a token with type keyword containing a string value
+        return token_create(&(struct token){.type = TOKEN_TYPE_KEYWORD, .sval = buffer_ptr(buf)});
+    }
 
     // Create and return new token of type identifier with string value.
     return token_create(&(struct token){.type = TOKEN_TYPE_IDENTIFIER, .sval = buffer_ptr(buf)});
